ac396350da93bd7bd7dcdc67c764cd6f386c187f
easywaldo <easywaldoji@gmail.com>
2022-03-20 02:43:06 +0900

    Entity Equality 에 대한 의미 테스트

--- entity_object.py
+++ entity_object.py
@@ -20,3 +20,42 @@ def test_barry_is_harry():
     assert harry is barry and barry is harry
     
 test_barry_is_harry()
+
+
+from dataclasses import dataclass
+from typing import NewType
+
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
+...
+
+class Batch:
+    def __init__(self, ref: Reference, sku: Sku, qty: Quantity):
+        self.sku = sku
+        self.reference = ref
+        self._purchased_quantity = qty
+        
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+
+def test_batch_is_same():
+    batch_first: Batch = Batch(Reference("odry"), Sku("color-yellow"), Quantity(100))
+    batch_two: Batch = Batch(Reference("odry"), Sku("color-green"), Quantity(100))
+    print(batch_first)
+    print(batch_two)
+    assert batch_first == batch_two
+    
+    batch_third = batch_first
+    batch_third.sku = Sku("color-pink")
+    print(batch_third)
+    print(batch_first)
+    assert batch_first == batch_third
+    
+test_batch_is_same()
