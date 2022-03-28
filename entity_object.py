115fc2bba7947e3e2cb0f2281bb5103d27793e39
easywaldo <easywaldoji@gmail.com>
2022-03-29 00:26:45 +0900

    Domain Exception raised test added

--- entity_object.py
+++ entity_object.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import NamedTuple
+from typing import List, NamedTuple
+
+import pytest
 
 @dataclass(frozen=True)
 class Name:
@@ -36,11 +38,19 @@ Sku = NewType("Sku", str)
 Reference = NewType("Reference", str)
 ...
 
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
 class Batch:
     def __init__(self, ref: Reference, sku: Sku, qty: Quantity):
         self.sku = sku
         self.reference = ref
         self._purchased_quantity = qty
+        self._allocations = set()
         
     def __eq__(self, other):
         if not isinstance(other, Batch):
@@ -49,6 +59,32 @@ class Batch:
 
     def __hash__(self):
         return hash(self.reference)
+    
+    def __gt__(self, other):
+        if self.qty is None:
+            return False
+        if other.qty is None:
+            return True
+        return self.qty > other.qty
+    
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+            
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
 
 
 def test_batch_is_same():
@@ -88,3 +124,26 @@ def test_barry_is_harry():
     
 
 test_barry_is_harry()
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+        )
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f'Out of stock {line.qty}')
+
+class OutOfStock(Exception):
+    pass
+
+
+def test_raise_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch('batch1', 'SMALL-FORK', 10)
+    allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+
+    with pytest.raises(OutOfStock, match='Out of stock 1'):
+        allocate(OrderLine('order2', 'SMALL-FORK', 1), [batch])
+    
+test_raise_out_of_stock_exception_if_cannot_allocate()
